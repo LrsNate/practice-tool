0d4168e3108c357ee6c6e7b97b7a04a2c4244ada
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2020-07-01 01:22:35 -0700

    Updates

---
+++ precache-manifest.78b6c7bf6cfcc0ea82c9b3de6c41a486.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "384db54fec79f1f337c0179fbd718b7d",
+    "url": "/practice-tool/index.html"
+  },
+  {
+    "revision": "ca86735eeeaf12100698",
+    "url": "/practice-tool/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a7304763fe63eb67da5c",
+    "url": "/practice-tool/static/js/2.87441ffe.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/practice-tool/static/js/2.87441ffe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca86735eeeaf12100698",
+    "url": "/practice-tool/static/js/main.2a39d732.chunk.js"
+  },
+  {
+    "revision": "bec40bdfd584bc39af36",
+    "url": "/practice-tool/static/js/runtime-main.a5d76572.js"
+  }
+]);
