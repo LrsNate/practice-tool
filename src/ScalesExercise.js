fb914ad0f7dae725167d65f1479ae50da98f3b97
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2020-07-01 15:44:21 -0700

    Install Prettier

--- src/ScalesExercise.js
+++ src/ScalesExercise.js
@@ -1,13 +1,21 @@
-import _ from 'lodash';
-import React from 'react';
+import _ from "lodash";
+import React from "react";
 
 const exercises = [
-  'C to F♯ (5ths)',
-  'C to G♭ (4ths)',
-  'F♯ to C (4ths)',
-  'G♭ to C (5ths)'
-]
+  "C to F♯ (5ths)",
+  "C to G♭ (4ths)",
+  "F♯ to C (4ths)",
+  "G♭ to C (5ths)",
+];
 
-const ScalesExercise = () => <h1>{_.sample(exercises)}</h1>
+class ScalesExercise extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>{_.sample(exercises)}</h1>
+      </React.Fragment>
+    );
+  }
+}
 
 export default ScalesExercise;
