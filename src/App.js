eb8a971918b894db0ece5988cbb8bae6517dfbea
Antoine Lafouasse <antoine.lafouasse@icloud.com>
2020-07-01 01:07:54 -0700

    Add reload button

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import './App.css';
 import ScalesExercise from "./ScalesExercise";
 
+const onReload = () => window.location.reload();
+const Reload = () => <button onClick={onReload}>Reload</button>
+
 function App() {
   return (
     <div className="App">
       <ScalesExercise/>
+      <Reload/>
     </div>
   );
 }
